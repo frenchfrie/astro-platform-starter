302f15d22d8217735d069ff1a734f3c043cb9553
Henri-Mayeul de Benque <henri-mayeul.debenque@doctolib.com>
2026-04-18 17:08:33 +0200

    Factorization in progress

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from 'astro:content';
+
+const gites = defineCollection({
+    type: 'data',
+    schema: ({ image }) => z.object({
+        id: z.string(),
+        title: z.string(),
+        description: z.string(),
+        pricePerNight: z.number(),
+        cleaningFee: z.number(),
+        maxGuests: z.number(),
+        deposit: z.number(),
+        abritelUrl: z.string().url(),
+        photos: z.array(z.object({
+            src: z.string(),
+            alt: z.string()
+        })),
+        features: z.array(z.string()), // ex: ["Piscine", "Jacuzzi", "Wifi"]
+    }),
+});
+
+export const collections = { gites };
